4f936e26281fdd73dcd5a828a8cbb6f1d20c0de6
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-05-24 18:32:37 +0500

    first commit

---
+++ public/index.js
@@ -0,0 +1,5 @@
+// Check if the user is on a mobile or tablet device
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    alert("This website is not yet optimized for mobile or tablet devices. Please view it on a desktop or laptop computer for the best experience.");
+  }
+  
